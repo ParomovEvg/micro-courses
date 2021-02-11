0096bc52d2d506d74e68d396eb61f22a91b134bd
Evgeny Paromov <evgeny.paromov@gmail.com>
2021-02-12 01:46:38 +0300

    added pub packages

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-console.log('changed')
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 8080);
